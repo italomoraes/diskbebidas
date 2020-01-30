adcae763d9dd9cf76912bed5574fa7a69acce4a8
Italo Moraes <italo_kz@hotmail.com>
2020-01-30 12:13:03 -0300

    Ajuste do horário

--- diskbebidas/settings.py
+++ diskbebidas/settings.py
@@ -108,13 +108,13 @@ LOGOUT_REDIRECT_URL = '/'
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
